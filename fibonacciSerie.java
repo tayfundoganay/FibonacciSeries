a8e5e7cad9852674a33dc22221c7de09a91c5a78
Tayfun DOGANAY <73904292+tayfundoganay@users.noreply.github.com>
2022-11-13 19:29:13 +0300

    Create fibonacciSerie.java

---
+++ fibonacciSerie.java
@@ -0,0 +1,26 @@
+package Loops;
+import java.util.Scanner;
+
+public class fibonacciSerie{
+    public static void main(String[] args) {
+        
+
+        int number;
+        int fibonacciNumber=0;
+        int result=1;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Kaç elemanlı fibonacci serisi istersiniz : ");
+        number = inp.nextInt();
+        System.out.print(number + " Elemanlı Fibonacci Serisi : \n");
+
+        for(int i =0;i<=(number-1);i++){
+            System.out.println((i+1) + ". fibonacci Sayısı : "+ fibonacciNumber);
+            result +=fibonacciNumber;
+            fibonacciNumber=result-fibonacciNumber;
+        }
+
+
+
+
+    }
+}
